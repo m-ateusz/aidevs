8540bb5ab2ed0e637f5b5223bbf40086440dd909
mateusz <mateusz.pabis@gmail.com>
2024-11-12 19:29:52 +0100

    Login XYZ

---
+++ s01_login_xyz.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+from aidevs import answer_question
+
+def solve_form_task():
+    # Fetch the page with the question
+    url = ... # FIXME: redacted for git due to security reasons, subdomain is xyz
+    
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # Extract question from paragraph with id=human-question
+    question = soup.find('p', id='human-question').text.strip()
+    
+    # Get answer using the local aidevs module
+    answer = answer_question(question, system_prompt='Answer using only one word or one number only!')
+    
+    # Prepare form data
+    form_data = {
+        'username': 'tester',
+        'password': '574e112a',
+        'answer': answer
+    }
+    
+    # Submit the form via POST
+    response = requests.post(url, data=form_data)
+    
+    return response.text
+
+if __name__ == "__main__":
+    result = solve_form_task()
+    print(result)
+
